8a7afc7bbabdf2ee7e0953b5cf6d62e71d6818cf
Wenxi <wenxi.chen@mail.utoronto.ca>
2015-07-29 15:38:30 -0400

    edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 		uniqueness: { case_sensitive: false }
 
 	has_secure_password
-	validates :password, length: { minimum: 6}, presence: true
+	validates :password, length: { minimum: 6}, presence: true, allow_nil: true
 
 	# Returns the hash digest of the given string.
 	def User.digest(string)
