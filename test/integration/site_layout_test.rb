e2095bd944e70ba72723657034ce0096f456cfd6
Wenxi <wenxi.chen@mail.utoronto.ca>
2015-08-08 19:13:17 -0400

    exercise

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+  end
 
-  test "layout links" do
+  test "layout links for non-logged-in users" do
   	get root_path
   	assert_template 'static_pages/home'
   	assert_select "a[href=?]", root_path, count: 2
@@ -13,8 +13,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select "a[href=?]", about_path
   	assert_select "a[href=?]", contact_path
   	assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
   	get signup_path
   	assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links for logged-in users" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
   
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -34,6 +34,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert session[:forwarding_url].blank?
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: {name: name, email: email,
@@ -44,6 +45,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+
+
+
 end
 
 
